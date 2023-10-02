62ffdc850cdbd6cea2234cf6b3b8d8b988c85c62
Renan Assensio <renanassensio05@gmail.com>
2023-10-02 20:58:28 -0300

    ap11(login): campos login e senha

---
+++ src/main/java/br/com/assensio/_usjt_psc_sistema_academico/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.assensio._usjt_psc_sistema_academico;
+
+/**
+ *
+ * @author 82318879
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
